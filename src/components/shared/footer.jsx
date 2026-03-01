e901a1e4dda53332e016274830f07f62b7c8f448
Manoj Sethi <manojsethi90@gmail.com>
2026-03-01 12:53:49 +0530

    changes

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -1,9 +1,7 @@
-// Footer.tsx
-
 const Footer = () => {
   return (
-    <footer className="bg-white/90 dark:bg-slate-900/90 backdrop-blur-md border-t border-slate-200 dark:border-slate-700">
-      <div className="w-full px-4 sm:px-6 lg:px-10 xl:px-12 2xl:px-16 py-6 text-center text-slate-700 dark:text-slate-300">
+    <footer className="bg-white/10 dark:bg-slate-900/10 backdrop-blur-xl border-t border-white/20 sticky bottom-0 z-[100]">
+      <div className="w-full px-4 sm:px-6 lg:px-10 xl:px-12 2xl:px-16 py-6 text-center text-black">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} WeatherApp. All rights reserved.
         </p>
